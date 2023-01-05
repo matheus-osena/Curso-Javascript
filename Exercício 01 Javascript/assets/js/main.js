c869af3945c66c794c4d496a2d996180e8673427
Matheus Sena <matheus_osena@hotmail.com>
2023-01-05 15:45:19 -0300

    Atualizando

---
+++ Exercício 01 Javascript/assets/js/main.js
@@ -0,0 +1,9 @@
+//Capturar o evento de submit do formulário
+const form = document.querySelector('#formulario')
+
+form.addEventListener('submit',function (e){
+    e.preventDefault()
+    console.log("Evento prevenido.")
+
+})
+
