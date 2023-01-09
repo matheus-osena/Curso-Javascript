b12363f222007829fbfa2628a1d08af749035ac1
Matheus Sena <matheus_osena@hotmail.com>
2023-01-09 14:59:51 -0300

    First

--- Exercício 01 Javascript/assets/js/main.js
+++ Exercício 01 Javascript/assets/js/main.js
@@ -3,7 +3,20 @@ const form = document.querySelector('#formulario')
 
 form.addEventListener('submit',function (e){
     e.preventDefault();
-    console.log("Evento prevenido.");
+    const inputPeso = e.target.querySelector("#input-peso");
+    const inputAltura = e.target.querySelector("#input-altura");
+    console.log(inputPeso);
 
+    const peso = Numeber(inputPeso.value);
 });
 
+function criaP (){
+    const p = document.createElement("P");
+    return p
+}
+function setResultado (msg){
+    const resultado = document.querySelector("#resultado");
+    resultado.innerHTML = " ";
+    const p = criaP();
+    resultado.appendChild(p);
+}
